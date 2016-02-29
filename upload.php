a3c56abf305b770a8a40e846ecfe7a41de2d2e08
Ravi Tiwari <ravi.pulp@gmail.com>
2016-02-29 11:47:02 +0530

    learning how to use github

---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+ $file  = time().'.jpg';
+ $img = str_replace(" ","+",$_REQUEST['queryString']);
+ $image_url = "images/";
+ 
+ 
+ 
+ if(strpos($img,'data:image/png') !== false)
+{
+    $img = str_replace('data:image/png;base64,', '', $img);
+  $img = str_replace(' ', '+', $img);
+}
+elseif((strpos($img,'data:image/jpg') !== false) || (strpos($img,'data:image/jpeg') !== false))
+{
+    $img = str_replace('data:image/jpeg;base64,', '', $img);
+  $img = str_replace(' ', '+', $img);
+}
+elseif(strpos($img,'data:image/bmp') !== false)
+{
+    $img = str_replace('data:image/bmp;base64,', '', $img);
+  $img = str_replace(' ', '+', $img);
+}
+
+$data    = base64_decode($img);
+$success = file_put_contents($image_url.$file, $data);
+ 
+
+ if($success){
+	echo "Success";
+ }
+?>
